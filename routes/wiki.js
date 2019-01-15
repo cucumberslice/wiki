282cc56c020a43b33b0ccb22e1c357abee1daed0
cucmberslice <ywblackberry@gmail.com>
2019-01-14 22:14:30 -0500

    can now query data from the database

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const {addPage} = require('../views')
+const {addPage, wikiPage} = require('../views')
 const { Page, User }= require('../models')
 
 
@@ -34,8 +34,23 @@ router.post('/', async (req,res,next) => {
         res.send(addPage())
     })
     
-    router.get('/:slug', (req,res, next) => {
-            res.send(`hit dynamic route at ${req.params.slug}`)
+    router.get('/:slug', async(req,res, next) => {
+
+        try {
+
+            const page = await Page.findOne({
+                where: {
+                    slug: req.params.slug
+                }
+            });
+           
+            res.send(wikiPage(page))
+        } catch (error) {
+            next(error)
+        }
+            // res.send(`hit dynamic route at ${req.params.slug}`)
         })
+
+    
 
 module.exports = router;
